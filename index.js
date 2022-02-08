689ff9ae8c9e0eab39d2b51ef6e46ce0ebe9b726
Denny Nguyen <leetosc@hotmail.com>
2022-02-08 00:31:56 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const axios = require("axios");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  axios
+    .get(
+      "https://raw.githubusercontent.com/awesome-selfhosted/awesome-selfhosted/master/README.md"
+    )
+    .then((response) => {
+      const text = response.data;
+      const parsed = text.replace(/## Table of contents[\s\S]+?(-----\s)/g, "");
+      res.send(parsed);
+    })
+    .catch((error) => {
+      res.send(error);
+    });
+});
+
+app.listen(port, () => console.log(`Server started on port ${port}`));

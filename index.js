9244f7657b1c8ae967e30b48207f02cbc187a38d
Denny Nguyen <leetosc@hotmail.com>
2022-02-08 12:09:56 -0600

    allow cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const axios = require("axios");
+const cors = require("cors");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
+app.use(cors());
 
 app.get("/", (req, res) => {
   axios
